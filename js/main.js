fa8221454a231a7da380de76c77c75c31478ca58
Camila Solis <camilasolis@new-host.home>
2016-11-13 18:05:40 -0300

    No funciona, pero algo es algo

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function agregaLista(){
+  var contenedorTarjetas = document.getElementById('contenedor');
+  var formulario = document.createElement('form');
+  var tarjeta = document.createElement('div');
+
+
+  var Texto = document.createElement('input');
+  Texto.setAttribute('type','text');
+  Texto.setAttribute('placeholder','Añadir una tarjeta');
+  Texto.setAttribute('id','inputTarea');
+  Texto.classList.add('textarea');
+  Texto.value = '';
+
+  var Boton = document.createElement('input');
+  Boton.setAttribute('type','button');
+  Boton.classList.add('btn');
+  Boton.classList.add('btn-primary');
+  Boton.classList.add('boton');
+  Boton.setAttribute('value','Guardar');
+  Boton.setAttribute('id','boton');
+
+  formulario.appendChild(Texto);
+  formulario.appendChild(Boton);
+  contenedorTarjetas.appendChild(formulario);
+
+  Boton.addEventListener('click', function(){
+    myform.reset();
+  })
+  
+}
